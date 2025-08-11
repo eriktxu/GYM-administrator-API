ac6a556ae948b4847e443864b89f419e9d02aabe
iSmo0ke <jesus.alt.2004@gmail.com>
2025-08-11 11:24:30 -0600

    Estilos y aviso de privacidad

--- routes/superadmin/superadmin.routes.js
+++ routes/superadmin/superadmin.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const superadminController = require('../../controllers/superadmin/superadmin.controller');
 const { verifyToken, authorizeRoles } = require('../../middleware/verifyToken');
 
-router.post('/registerGimnasio', verifyToken, superadminController.registerGimnasio);
+router.post('/registerGimnasio', superadminController.registerGimnasio);
 router.post('/login', superadminController.login);
 
 
