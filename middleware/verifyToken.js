d7cb16ea1bbe43534a7c7385eb3ca5c16041d780
erik <eriktrujillo0001@gmail.com>
2025-08-11 04:08:41 -0600

    logs token

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,26 +2,30 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const verifyToken = (req, res, next) => {
+    console.log('verifyToken middleware ejecutado'); // <-- log inmediato para saber si entra
+
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
+        console.log('Token no proporcionado');
         return res.status(401).json({ message: 'Acceso denegado. Token no proporcionado.' });
     }
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        console.log('verifyToken - decoded token payload:', decoded); // <--- aquí
+        console.log('verifyToken - decoded token payload:', decoded);
         req.user = decoded;
         next();
     } catch (error) {
+        console.log('Token inválido o expirado');
         return res.status(403).json({ message: 'Token inválido o expirado.' });
     }
 };
 
-
 const authorizeRoles = (...allowedRoles) => {
     return (req, res, next) => {
+        console.log('authorizeRoles middleware ejecutado');
         console.log('authorizeRoles - req.user:', req.user);
         console.log('authorizeRoles - allowedRoles:', allowedRoles);
         if (!req.user || !allowedRoles.includes(req.user.rol_id)) {
@@ -31,7 +35,6 @@ const authorizeRoles = (...allowedRoles) => {
         next();
     };
 };
-
 
 module.exports = {
         verifyToken, 
